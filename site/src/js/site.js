6b60413cf836199014d79950cdba59757c12ebbc
raz0red <raz0red@gmail.com>
2020-04-14 11:38:04 -0700

    Added buttons to specify local and remote files

--- site/src/js/site.js
+++ site/src/js/site.js
@@ -1,5 +1,7 @@
 import { zip } from "./zip.js"
 import css from '../css/site.css'
+import cloudDownloadImgSrc from "../images/cloud-download.svg"
+import folderOpenImgSrc from "../images/folder-open.svg"
 
 if (!String.prototype.endsWith) {
   String.prototype.endsWith = function (search, this_len) {
@@ -19,8 +21,7 @@ var snackbarEl = null;
 var errorMessageEl = null;
 var errorTextEl = null;
 
-var isGitHub = (
-  window.location.hostname.toLowerCase() == 'raz0red.github.io');
+var isGitHub = (window.location.hostname.toLowerCase() == 'raz0red.github.io');
 
 var errorHandler = function (error, logToConsole) {
   if (logToConsole == undefined) {
@@ -33,7 +34,6 @@ var errorHandler = function (error, logToConsole) {
 }
 
 function unzip(file, success, failure) {
-  var error = null;
   failure = failure || errorHandler;
 
   function entryProcessor(entries) {
@@ -103,9 +103,13 @@ function startEmulation(blob) {
   );
 }
 
-function loadRomFromUrl(url) {
+function loadFromUrl(url) {
   var start = Date.now()
   var minWait = 750;
+  var urlLower = url.toLowerCase();
+  var forceList  = (
+    urlLower.endsWith(".json") || (urlLower.indexOf(".json?") != -1));
+
   showMessage('Loading...')
 
   var xhr = new XMLHttpRequest();
@@ -115,7 +119,7 @@ function loadRomFromUrl(url) {
     try {
       if (xhr.status >= 300 || xhr.stats < 200) {
         throw xhr.status + ": " + xhr.statusText;
-      } else {
+      } else if (!romList.loadListFromFile(xhr.response) && !forceList) {
         startEmulation(xhr.response);
       }
     } catch (e) {
@@ -142,7 +146,7 @@ var RomList = function (selects) {
       var select = selects[iidx];
       select.onchange = function () {
         var value = select.value;
-        loadRomFromUrl(value);
+        loadFromUrl(value);
         this.blur();
         for (var i = 0; i < selects.length; i++) {
           if (selects[i] != select) {
@@ -313,14 +317,25 @@ var RomList = function (selects) {
     loadList(json, false);
   }
 
+  this.loadListFromFile = function (file) {
+    var fname = file.name ? file.name.toLowerCase() : "";
+    var ftype = file.type ? file.type.toLowerCase() : "";
+    if (fname.endsWith(".json") ||
+      ftype.includes("text") || ftype.includes("json")) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        romList.loadListFromJson(event.target.result);
+      };
+      reader.readAsText(file);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   this.loadListFromUrl = function (romShareUrl) {
     loadList(romShareUrl, true);
   }
-}
-
-function getRequestParameter(name) {
-  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
-    return decodeURIComponent(name[1]);
 }
 
 function fileDropHandler(ev) {
@@ -332,24 +347,15 @@ function fileDropHandler(ev) {
       var item = ev.dataTransfer.items[i];
       if (item.kind === 'file') {
         file = item.getAsFile();
-        if (file.name.toLowerCase().endsWith(".json")) {
-          var reader = new FileReader();
-          reader.onload = function (event) {
-            romList.loadListFromJson(event.target.result);
-          };
-          reader.readAsText(file);
-          file = null; // not a rom
+        if (romList.loadListFromFile(file)) {
+          // Don't start emulation, was a rom list
+          file = null;
         }
         break;
       } else if (item.kind === 'string' &&
         (item.type.match('^text/uri-list') || item.type.match('^text/plain'))) {
         item.getAsString(function (url) {
-          var urlLower = url.toLowerCase();
-          if (urlLower.endsWith(".json") || (urlLower.indexOf(".json?") != -1)) {
-            romList.loadListFromUrl(url);
-          } else {
-            loadRomFromUrl(url);
-          }
+          loadFromUrl(url);
         });
       }
     }
@@ -384,7 +390,38 @@ function hideErrorMessage() {
   errorMessageEl.classList.remove('show');
 }
 
+function createImageButton(divId, imgSrc, title, allowTab, callback) {
+  var div = document.getElementById(divId);
+  div.className = "controls-button";
+  if (callback) {
+    div.onclick = callback;
+  }
+  var label = document.createElement("label");
+  div.appendChild(label);
+  if (allowTab) {
+    label.setAttribute("tabindex", "0");
+    label.onmousedown = function (e) { e.preventDefault(); };
+    if (callback) {
+      div.onkeydown = function (e) {
+        var code = e.which;
+        if ((code === 13) || (code === 32)) {
+          callback();
+        }
+      };
+    }
+  }
+  var img = document.createElement("img");
+  label.appendChild(img);
+  img.className = "controls-button__img";
+  img.setAttribute("draggable", "false");
+  img.setAttribute("src", imgSrc);
+  img.setAttribute("title", title);
+
+  return { "div": div, "label": label, "img": img };
+}
+
 function addElements() {
+  // The fullscreen container
   var parent = document.getElementById('js7800__fullscreen-container');
 
   // Snackbar (Loading messages, etc.)
@@ -406,43 +443,90 @@ function addElements() {
   closeButtonEl.innerHTML = '&times;';
   errorTextEl = document.createElement('span');
   errorMessageEl.appendChild(errorTextEl);
+
+  //
+  // Buttons
+  //
+
+  // Remote file button
+  createImageButton("select-remote-file", cloudDownloadImgSrc,
+    "Select Remote File", true,
+    function () { 
+      var url = prompt("Enter the URL of a remote ROM file or ROM list");
+      if (url) {
+        var trimmed = url.trim();
+        if (trimmed.length > 0) {
+          loadFromUrl(trimmed);
+        }
+      }    
+    });
+
+  // Local file button
+  var localFileButton = createImageButton("select-local-file", folderOpenImgSrc,
+    "Select Local File", false, null);  
+  var label = localFileButton.label;
+  var fileInput = document.createElement("input");
+  label.className = "controls-button__upload";
+  label.appendChild(fileInput);
+  fileInput.setAttribute("type", "file");
+  fileInput.setAttribute("accept", ".a78, .bin, .zip, .json");
+  fileInput.addEventListener("change", function () {
+    var fileList = this.files;
+    for (var i = 0; i < fileList.length; i++) {
+      var f = fileList[i];
+      if (!romList.loadListFromFile(f)) {
+        startEmulation(f);
+      }
+      break;
+    }
+  }, false);
 }
 
-function init(in7800) {
-  js7800 = in7800;
-  var main = js7800.Main;
+function createFullscreenSelect() {
   var cbar = js7800.ControlsBar;
   var util = js7800.Util;
   var events = js7800.Events;
 
-  main.setMessageHandler(showMessage);
-  main.setErrorHandler(errorHandler);
-
-  // Cartridge select
-  var selectId = 'cartselect__select';
-  var select = document.getElementById(selectId);
-  if (!select) {
-    throw "Unable to find select element with id: " + selectId;
-  }
-
-  // Full screen cartridge select
+  // Create full screen cartridge select
   var fsSelect = document.createElement("div");
   var fsSelectSel = document.createElement("select");
   fsSelect.appendChild(fsSelectSel);
+
+  // Create the component in the command bar
   var fsSelectComp = new cbar.Component();
   util.addProps(fsSelectComp, {
     getClass: function () { return "fsselect"; },
     doCreateElement: function () { return fsSelect; }
   });
   cbar.getGroup(1).addChildAtIndex(2, fsSelectComp);
+
+  // Listen for full screen change vents
   var fullscreenListener = new events.Listener("fullscreen");
   fullscreenListener.onEvent = function (isFullscreen) {
     fsSelect.style.display = isFullscreen ? "flex" : "none";
   }
   events.addListener(fullscreenListener);
 
+  return fsSelectSel;
+}
+
+function getRequestParameter(name) {
+  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+    return decodeURIComponent(name[1]);
+}
+
+function init(in7800) {
+  js7800 = in7800;
+  var main = js7800.Main;
+
+  // Set message handlers for js7800 component
+  main.setMessageHandler(showMessage);
+  main.setErrorHandler(errorHandler);
+
   // Rom list component
-  romList = new RomList([select, fsSelectSel]);
+  romList = new RomList([
+    document.getElementById('cartselect__select'), 
+    createFullscreenSelect()]);
 
   // Initialize js7800
   main.init('js7800__target');
@@ -450,39 +534,37 @@ function init(in7800) {
   // Add display elements
   addElements();
 
+  // Register drop handlers
+  var ignore = function (event) { event.preventDefault(); }
+  var body = document.body;
+  body.addEventListener("drop", fileDropHandler);
+  body.addEventListener("dragdrop", fileDropHandler);
+  body.addEventListener("dragenter", ignore);
+  body.addEventListener("dragover", ignore);
+
   //
   // Handle request parameters
   //
 
-  // rom list
+  // ROM list
   var rlist = getRequestParameter("romlist");
   if (!rlist) {
     rlist = 'roms/romlist-homebrew.json';
   }
   romList.loadListFromUrl(rlist);
 
-  // rom
+  // ROM
   var rom = getRequestParameter("rom");
   if (rom) {
-    loadRomFromUrl(rom);
+    loadFromUrl(rom);
   }
 
-  // log fps
+  // Log FPS
   var logFps = getRequestParameter("fps");
   if (logFps) {
     logFps = logFps.toLowerCase();
     main.setLogFps(logFps === "1" || logFps == "true");
   }
-
-  // Register drop handlers
-  var ignore = function (event) {
-    event.preventDefault();
-  }
-  var body = document.body;
-  body.addEventListener("drop", fileDropHandler);
-  body.addEventListener("dragdrop", fileDropHandler);
-  body.addEventListener("dragenter", ignore);
-  body.addEventListener("dragover", ignore);
 }
 
 export {
