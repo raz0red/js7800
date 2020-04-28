34aa00de6027e1cb74070506372e6d7af92d2f3a
raz0red <raz0red@gmail.com>
2020-04-28 08:51:45 -0700

    Added path to encode/decode

--- site/src/js/hs-encode-decode.js
+++ site/src/js/hs-encode-decode.js
@@ -1,4 +1,3 @@
-
 function dumpDetails(sram) {}
 function encode(sram, scores) {}
 function generateTestSram(sram) {}
@@ -8,3 +7,5 @@ export {
   encode,
   generateTestSram
 }
+
+//export * from '../../../../js7800-server/js/site/src/js/hs-encode-decode.js'
