5dea3ea2e0e3580899296736ed5037ccd8b9ad11
raz0red <raz0red@gmail.com>
2020-05-04 00:03:39 -0700

    Starting to implement info dialog

--- site/src/js/help-dialog.js
+++ site/src/js/help-dialog.js
@@ -7,8 +7,42 @@ var TabSet = DialogModule.TabSet;
 var Tab = DialogModule.Tab;
 var addProps = Util.addProps;
 
+// 
+// Help Tab
+//
+
+function HelpTab(title) {
+  Tab.call(this, title);
+}
+HelpTab.prototype = Object.create(Tab.prototype);
+addProps(HelpTab.prototype, {  
+  root: null,
+  loaded: false,
+  onTabShow: function() {
+    if (!this.loaded) {
+      this.loaded = true; 
+      // var that = this;
+      // setTimeout(function() {
+      //   that.root.innerHTML = "";
+      // }, 5000);
+    }
+  },  
+  createTabContent: function (rootEl) { 
+    this.root = rootEl;
+    var parent = document.createElement("div");
+    parent.className = "loader-container";    
+    var loader = document.createElement("div");
+    loader.className = "loader";
+    parent.appendChild(loader);
+    rootEl.appendChild(parent);
+  },
+});
+
+var testTab = new HelpTab("Overview");
+
 var tabSet = new TabSet();
 tabSet.addTab(new AboutTab(), true);
+//tabSet.addTab(testTab);
 
 //
 // Help dialog
