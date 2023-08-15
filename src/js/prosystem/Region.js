72f31c03cfa083bdd9776bf1448dc406b2bfa8b3
Mike Saarna <msaarna@gmail.com>
2023-08-15 23:17:42 +0000

    change native samplerate, when possible, to avoid aliasing

--- src/js/prosystem/Region.js
+++ src/js/prosystem/Region.js
@@ -27,6 +27,7 @@ import * as ProSystem from "./ProSystem.js"
 import * as Cartridge from "./Cartridge.js"
 import * as Maria from "./Maria.js"
 import * as Palette from "./Palette.js"
+import * as Webaudio from "../web/audio.js"
 import { Rect } from "./Rect.js"
 
 import N257_PAL_B from '../../palettes/NTSC/JS7800_NTSC_257_Cool-DK.pal'
@@ -270,6 +271,7 @@ function region_Reset() {
        Palette.Load(PAL_PAL_INDEX[palIdx] /*REGION_PALETTE_PAL*/);  // Added check for default - bberlin
     ProSystem.SetFrequency(REGION_FREQUENCY_PAL);
     ProSystem.SetScanlines(REGION_SCANLINES_PAL);
+    Webaudio.reinit(REGION_FREQUENCY_PAL*REGION_SCANLINES_PAL*2);
     //#ifndef WII    
     //tia_size = 624;
     //pokey_size = 624;
@@ -284,6 +286,7 @@ function region_Reset() {
        Palette.Load(NTSC_PAL_INDEX[palIdx] /*REGION_PALETTE_NTSC*/);  // Added check for default - bberlin
     ProSystem.SetFrequency(REGION_FREQUENCY_NTSC);
     ProSystem.SetScanlines(REGION_SCANLINES_NTSC);
+    Webaudio.reinit(REGION_FREQUENCY_NTSC*REGION_SCANLINES_NTSC*2);
     //#ifndef WII    
     //tia_size = 524;
     //pokey_size = 524;
